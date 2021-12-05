34cc34168a52e8e4713d14c49ea113c2b00756bf
jackomiel <94228882+jackomiel@users.noreply.github.com>
2021-12-05 01:24:58 +0100

    Update INT 2 sonarQube01.java

--- INT 2 sonarQube01.java
+++ INT 2 sonarQube01.java
@@ -1,5 +1,5 @@
-
-public class sonarQube01 {
+package org.example;
+public class SonarQube01 {
     String addHeader(String stringToAdd, int x, int y){
         return String.format("Added Header %s %d %d", stringToAdd, x, y);
     }
